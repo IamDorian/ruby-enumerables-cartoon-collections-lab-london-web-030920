24a3dfc27b08d5542fea49551371e1003408e6c8
Dorian Calin <dorin.m.calin@gmail.com>
2020-02-25 04:13:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,11 @@ def long_planeteer_calls# code an argument here
   # Your code here
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find { |cheese|
+    cheese_types.include?(cheese)}
 end
+contains_gouda = ["potato", "gouda", "camembert"]
+find_the_cheese(contains_gouda)
